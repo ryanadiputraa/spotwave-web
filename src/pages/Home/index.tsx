58ddae8363185fb3ed748b9c4abcb5699414d515
Ryan Adi Putra <ryannadiputraa@gmail.com>
2024-07-04 16:03:31 +0800

    Revert "chore: change visible app name"
    This reverts commit c01ce711b8fa094b137061ae6cc41ace07543177.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,8 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from 'react-router-dom';
 
-import { Button } from "../../components/Button";
-import { Footer } from "../../components/Footer";
-import { BASE_API_URL, getAccessTokens } from "../../utils";
+import { Button } from '../../components/Button';
+import { Footer } from '../../components/Footer';
+import { BASE_API_URL, getAccessTokens } from '../../utils';
 
 const Home = () => {
   const navigate = useNavigate();
@@ -10,7 +10,7 @@ const Home = () => {
 
   const onLogin = () => {
     if (tokens?.accessToken.length) {
-      navigate("/dashboard");
+      navigate('/dashboard');
       return;
     }
     window.location.href = `${BASE_API_URL}/oauth/login`;
@@ -19,12 +19,12 @@ const Home = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-white to-accent">
       <header className="flex items-center justify-between py-2 px-[2%] sm:px-6">
-        <Link to={"/"} className="flex items-center justify-center gap-2">
-          <img className="w-10" src="/logo.svg" alt="waveey-logo" />
-          <h1 className="font-bold">Waveey</h1>
+        <Link to={'/'} className="flex items-center justify-center gap-2">
+          <img className="w-10" src="/logo.svg" alt="spotwave-logo" />
+          <h1 className="font-bold">Spotwave</h1>
         </Link>
         <Button variant="primary" classNames="btn btn-primary" onClick={onLogin}>
-          {tokens?.accessToken?.length ? "Dashboard" : "Login"}
+          {tokens?.accessToken?.length ? 'Dashboard' : 'Login'}
         </Button>
       </header>
       <main className="px-[2%] py-2 min-h-[87vh] flex flex-col justify-center">
